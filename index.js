e1530ac47e3e508416784b9870aec0d9ff9b72b6
yseto <yseto@users.noreply.github.com>
2021-02-04 13:59:25 +0900

    staging-release-update-action

--- index.js
+++ index.js
@@ -1,18 +1,50 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const github = require('@actions/github');
+const read = require('fs-readdir-recursive');
 
-
-// most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const fs = require('fs').promises;
+    const path = require('path');
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    const token = core.getInput('github-token');
+    const directory = core.getInput('directory');
+    const tag = core.getInput('tag');
 
-    core.setOutput('time', new Date().toTimeString());
+    const octokit = github.getOctokit(token);
+
+    const context = github.context;
+    const { repo: { owner, repo }, ref } = context;
+
+    const commit = await octokit.repos.getCommit({ owner, repo, ref });
+
+    const staging = await octokit.repos.getReleaseByTag({ owner, repo, tag });
+    await octokit.repos.deleteRelease({ owner, repo, release_id: staging.data.id });
+
+    const newStaging = await octokit.repos.createRelease({
+      owner,
+      repo,
+      tag_name: tag,
+      prerelease: true,
+      body: commit.data.commit.message,
+    });
+
+    const artifacts = read('.', () => true, [], directory);
+
+    core.startGroup('Assets')
+    for (let file of artifacts) {
+      core.info('uploading ' + file);
+
+      await octokit.repos.uploadReleaseAsset({
+        owner, repo,
+        release_id: newStaging.data.id,
+        name: path.basename(file),
+        data: await fs.readFile(file),
+      });
+    }
+    core.endGroup()
+
+    core.info("\u001b[1mStaging Release: " + newStaging.data.html_url);
   } catch (error) {
     core.setFailed(error.message);
   }
