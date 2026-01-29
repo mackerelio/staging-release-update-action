81705d1cb7d353f5904a1c48b8553e013a1a0b00
yseto <yseto@users.noreply.github.com>
2026-01-29 12:34:23 +0900

    esm support

--- index.test.js
+++ index.test.js
@@ -1,18 +1,18 @@
-const io = require('@actions/io');
-const fs = require('fs');
+import { rmRF, mkdirP } from '@actions/io';
+import { writeFile } from 'fs';
 
-const read = require('fs-readdir-recursive');
+import read from 'fs-readdir-recursive';
 
 test('read assets', async () => {
-  await io.rmRF('tmp-readdir/');
-  await io.mkdirP('tmp-readdir/1/2/');
+  await rmRF('tmp-readdir/');
+  await mkdirP('tmp-readdir/1/2/');
   const files = [
     'tmp-readdir/1.txt',
     'tmp-readdir/1/2.txt',
     'tmp-readdir/1/2/3.txt',
   ];
   for (let filename of files) {
-    fs.writeFile(filename, 'Hello', (err) => {
+    writeFile(filename, 'Hello', (err) => {
       if (err) throw err;
     });
   }
